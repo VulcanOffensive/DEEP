9ce6e00cdd81ff5e67da112bdaba6401ea6b058d
Ygor Simões <ygorsimoes@hotmail.com>
2023-08-26 16:55:51 -0300

    refactor: rename main class

--- src/main/java/com/sanctumoffensive/Startup.java
+++ src/main/java/com/sanctumoffensive/Startup.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeepApplication {
-
+public class Startup {
     public static void main(String[] args) {
-        SpringApplication.run(DeepApplication.class, args);
+        SpringApplication.run(Startup.class, args);
     }
-
 }
